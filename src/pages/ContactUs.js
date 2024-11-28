f5c6e4d7baea59f660d4d5fa05f448d2d7740f19
Babatee <bossbabatee@gmail.com>
2024-11-28 12:44:54 +0100

    First commit

---
+++ src/pages/ContactUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ContactUs = () => {
+    return (
+        <div>
+            <h2>Contact Us</h2>
+            <p>Feel free to reach out to us!</p>
+        </div>
+    );
+};
+
+export default ContactUs;
